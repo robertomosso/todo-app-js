d3cf3b8b5c26fca05e9167f5eda4fb4194100b46
robmosso <roberto.mosso@eng.it>
2024-11-30 11:20:05 +0100

    wip edit todo, modified style

--- main.js
+++ main.js
@@ -5,23 +5,41 @@ const todoList = document.querySelector("#todo-list");
 const addTodoButton = document.querySelector("#add-button");
 
 addTodoButton.addEventListener("click", () => {
-  const todoLi = document.createElement("li");
-  const todoLiDiv = document.createElement("div");
-  const todoLiSpan = document.createElement("span");
-  const todoLiButton = document.createElement("button");
 
+  const todoLi = document.createElement("li");
+  todoLi.classList.add('todoLiDiv');
+
+  const todoLiCheckbox = document.createElement("input");
+  const todoLiSpan = document.createElement("span");
+  const todoLiEditButton = document.createElement("button");
+  const todoLiDeleteButton = document.createElement("button");
+
+  todoLiCheckbox.type = 'checkbox';
+  todoLiCheckbox.addEventListener('change', () => {
+    console.log('ckeck', todoLiCheckbox.checked);
+    todoLiSpan.classList.toggle('toggle-item');
+  })
+
+  todoLiSpan.classList.add('todoLiDiv');
   todoLiSpan.innerText = input.value;
   input.value = "";
-  todoLiButton.innerText = "Remove";
 
-  todoLiButton.addEventListener("click", () => {
-    console.log("remove");
+  todoLiEditButton.innerHTML = `<i class="bi bi-pencil-square"></i>`;
+  todoLiEditButton.addEventListener("click", (e) => {
+    console.log(e, todoLiSpan);
+    
+    // input.value = e.target
+  });
+
+  todoLiDeleteButton.innerHTML = `<i class="bi bi-x-circle-fill"></i>`;
+  todoLiDeleteButton.addEventListener("click", () => {
     todoList.removeChild(todoLi);
   });
 
-  todoLiDiv.appendChild(todoLiSpan);
-  todoLiDiv.appendChild(todoLiButton);
+  todoLi.appendChild(todoLiCheckbox);
+  todoLi.appendChild(todoLiSpan);
+  todoLi.appendChild(todoLiEditButton);
+  todoLi.appendChild(todoLiDeleteButton);
 
-  todoLi.appendChild(todoLiDiv);
   todoList.appendChild(todoLi);
 });
